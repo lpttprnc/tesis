cee9e99e912f0090c7f7b46d28c8f9986179233f
lpttprnc <98081190+lpttprnc@users.noreply.github.com>
2022-07-02 16:08:58 +0700

    Update otomasi.py
    yang wajib dicek:
    - f_init = np.linspace(0,10,1000)
    - ada 5 array buat append
    - file = open('gravity_models/alpha_attractor_canonical.ini', 'r') directory sesuai
    - parameter potensial sesuai yang mau dirunning
    - data = pd.read_csv ('directory file.dat') directory sesuai, append 3 parameter: densitas, tekanan, nilai medan saat ini
    - data2 = pd.read_csv ('directory file.dat') directory sesuai, append 2 parameter: parameter densitas dari budget energy dan parameter potensial c
    - except: ada 5 parameter "NaN"
    - output_dat = pd.DataFrame({'header':parameter}) ada 5 parameter
    - output_dat.to_csv directory sesuai
    
    opsional tapi cek aja:
    - import time
    - waktu start dan stop
    - durasi running
    - index False di dat.to.csv

--- otomasi.py
+++ otomasi.py
@@ -8,18 +8,19 @@ def run_bash_script(script):
         a=0
         os.system("{} < running".format(script))
 
-f_init = np.linspace(0,10,10)
+f_init = np.linspace(0,10,1000) #CEK!!! 1000
 
-rho_smg = []
+rho_smg = [] #CEK ADA 5 ARRAY
 p_smg = []
+phi_smg = []
 wanted = []
 par_c = []
 
 start = time.time()
 for f in f_init:
-    file = open('gravity_models/alpha_attractor_canonical.ini', 'r')
+    file = open('gravity_models/alpha_attractor_canonical.ini', 'r') #CEK!!!
     text = file.readlines()
-
+#######################################################CEKKKKKK!!!!!!!#######################################################
     text[17] = 'parameters_smg =  1e-100, \t  {}, 8.530,  0,  3.140, 4.233\n'.format(f)  #f_prime_ini, f_ini, alpha, c^2, p, n
     #text[17] = 'parameters_smg =  1e-100, \t  {}, 8.530,  0,  3.140, 4.233\n'.format(f) #f_prime_ini, f_ini, alpha, c^2, p, n default
 
@@ -28,21 +29,23 @@ for f in f_init:
             f.write(t)
     try:
         run_bash_script("./class alpha_attractor_canonical.ini")
-        data = pd.read_csv('gravity_models/output/alpha_attractor_canonical_background.dat',delimiter='\s+',skiprows=3)
+        data = pd.read_csv('gravity_models/output/alpha_attractor_canonical_background.dat',delimiter='\s+',skiprows=3) #CEK DIRECTORY
         rho_smg.append(data.iloc[-1,15])
         p_smg.append(data.iloc[-1,16])
+        phi_smg.append(data.iloc[-1,25]) #CEK UDAH ADA INI BELUM!!!!!!!!!!!
         
-        data2 = pd.read_csv('gravity_models/output/alpha_attractor_canonical_nebengparameters.dat',delimiter='\s+',skiprows=0)
+        data2 = pd.read_csv('gravity_models/output/alpha_attractor_canonical_nebengparameters.dat',delimiter='\s+',skiprows=0) #CEK DIRECTORY
         wanted.append(data2.iloc[0,0]) #Omega smg wanted
         par_c.append(data2.iloc[0,1])
-    except:
+    except: #CEK ADA 5
         rho_smg.append("NaN")
         p_smg.append("NaN")
+        phi_smg.append("NaN") 
         wanted.append("NaN") #Omega smg wanted
         par_c.append("NaN")
 
-output_dat = pd.DataFrame({'f_init':f_init,'rho_smg':rho_smg,'p_smg':p_smg,'wanted_value':wanted,'c':par_c})
-output_dat.to_csv('gravity_models/output/output_data.csv',index=False)
+output_dat = pd.DataFrame({'f_init':f_init,'rho_smg':rho_smg,'p_smg':p_smg,'wanted_value':wanted,'c':par_c}) #CEK ADA 5
+output_dat.to_csv('gravity_models/output/output_data.csv',index=False) #CEK DIRECTORY
 
 stop = time.time()
 print("Running duration: ", stop-start, " seconds.")
